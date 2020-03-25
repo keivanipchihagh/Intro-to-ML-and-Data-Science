4706583c238f467151124ebee50e84c6d40ec7d2
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2020-03-25 17:24:21 +0430

    Create UnitConvertor_SupervisedLearning.py

---
+++ UnitConvertor_SupervisedLearning/UnitConvertor_SupervisedLearning.py
@@ -0,0 +1,33 @@
+# Simple supervised learning algorithm (Perceptron - CNN) to convert celsius to fahrenheit
+
+import numpy, math, random
+
+bias = 1
+learningRate = 0.00001
+weights = [random.random() for i in range(2)]
+
+def Perceptron(inputs, target):
+
+    # calculate the overall guess score
+    output = (weights[0] * inputs)
+    output += (bias * weights[1])
+
+    # No-need for activation function here
+
+    # Train the model like this
+    error = target - output
+    weights[0] += (error * learningRate * inputs)
+    weights[1] += (error * learningRate * bias)
+
+# Train perceptron for a lot of cycles
+for i in range(1000000):
+    Perceptron(0, 32)
+    Perceptron(8, 46.4)
+    Perceptron(15, 59)
+    Perceptron(22, 71.6)
+
+# 38 celsius equals to??
+output = (weights[0] * 38)
+output += (bias * weights[1])
+print(output)
+print(weights)
