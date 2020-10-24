ab2f4fe91b8a2404d3e34970329224b4220cfb85
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2020-10-24 14:17:26 +0330

    Update sudoku-solver.py

--- Projects/Sudoku Solver/sudoku-solver.py
+++ Projects/Sudoku Solver/sudoku-solver.py
@@ -34,3 +34,54 @@ def get_empty(board):
         for j in range(len(board[i])):
             if board[i][j] == 0:
                 return (i, j)
+    return None
+
+# Validation
+def valid(baord, number, position):
+    # Check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+    
+    # Check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # Check cube
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+    
+    return True
+
+def solve(board):
+
+    empty = get_empty(board = board)
+    if not empty:
+        return True
+    else:
+        row, column = empty
+
+    for i in range(1, 10):
+        if valid(board, i, (row, column)):
+            board[row][column] = i
+
+            if solve(board = board):
+                return True
+            board[row][column] = 0
+    
+    return False
+
+# Main
+print('Initial Board:')
+print_board(board = board)
+
+solve(board = board)
+
+print('Solved Board:')
+print_board(board = board)
