900c8da4bef2953221d0f5acbb6da4ed7894cb0e
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2020-10-25 14:19:53 +0330

    Optimizing the layer

--- Neural Network from skratch/neuron.py
+++ Neural Network from skratch/neuron.py
@@ -1,20 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-inputs = [1, 2, 3, 2.5]
-weights = [[0.2, 0.8, -0.5, 1.0],
-		   [0.5, -0.91, 0.26, -0.5],
-		   [-0.26, -0.27, 0.17, 0.87]]
+np.random.seed(0)
 
-biases = [2, 3, 0.5]
+X = [[1, 2, 3, 2.5],
+		  [2.0, 5.0, -1.0, 2.0],
+		  [-1.5, 2.7, 3.3, -0.8]]
 
+class Layer_Dense:
+	def __init__(self, n_inputs, n_nourons):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_nourons)
+		self.biases = np.zeros((1, n_nourons))
+	def forward(self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
 
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-	neuron_output = 0
-	for neuron_input, weight in zip(inputs, neuron_weights):
-		neuron_output += (neuron_input * weight)
-	neuron_output += neuron_bias
-	layer_outputs.append(neuron_output)
+dense_layer_1 = Layer_Dense(4, 5)
+dense_layer_2 = Layer_Dense(5, 2)
 
-print(layer_outputs)
+dense_layer_1.forward(X)
+dense_layer_2.forward(dense_layer_1.output)
+
+print(dense_layer_2.output)
